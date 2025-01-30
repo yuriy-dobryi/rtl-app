3fc61f0f0b90d33cc1bb1cfd00e64d6443a15c5e
yuriy.d <yuriy.dobryi@pettersonapps.com>
2025-01-30 11:31:31 +0200

    added other configs

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  jsxSingleQuote: false,
+  singleAttributePerLine: false,
+  arrowParens: 'avoid',
+};
